12e1da847eb764452f11241a373229a8f2f969e4
jpashayev <jpashayev@aol.com>
2022-04-05 17:50:14 -0700

    first save

---
+++ planet-rank-frontend/planet-rank/src/app/pic-of-the-day/pic-of-the-day.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pic-of-the-day',
+  templateUrl: './pic-of-the-day.component.html',
+  styleUrls: ['./pic-of-the-day.component.css']
+})
+export class PicOfTheDayComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
