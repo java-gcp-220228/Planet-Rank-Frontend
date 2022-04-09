8838154964dca8055c53bdc5578fffa838ab79ac
jpashayev <jpashayev@aol.com>
2022-04-09 16:47:38 -0700

    style changes, structure update, login framework setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ImageGalleryComponent } from './image-gallery/image-gallery.component';
-import { PicOfTheDayComponent } from './pic-of-the-day/pic-of-the-day.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
-import { ExoplanetCardsComponent } from './exoplanet-cards/exoplanet-cards.component';
-import { LoginComponent } from './login/login.component';
-import { LogoutComponent } from './logout/logout.component';
+import { ImageGalleryComponent } from './components/image-gallery/image-gallery.component';
+import { PicOfTheDayComponent } from './components/pic-of-the-day/pic-of-the-day.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { ExoplanetCardsComponent } from './components/exoplanet-cards/exoplanet-cards.component';
+import { LoginComponent } from './components/login/login.component';
+import { LogoutComponent } from './components/logout/logout.component';
 
 @NgModule({
   declarations: [
