7935d4c71733a4ed300c0ac7bf6a697594dda9bd
jpashayev <jpashayev@aol.com>
2022-04-10 08:48:48 -0700

    login page sends request with Not Supported Type

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -7,11 +7,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  username!: string;
+  password!: string;
+  token!: string;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  // const loginButton = 
+  async loginUser() {
+    const postLoginURL = 'http://localhost:8080/login';
+    const jsonString = JSON.stringify({
+      "username": this.username,
+      "password": this.password
+    });
+
+    console.log(jsonString);
+
+    let res = await fetch(postLoginURL, {
+      method: 'POST',
+      body: jsonString,
+  });
+
+    this.token = res.headers.get('token')!;
+    localStorage.setItem('jwt', this.token);
+    
+    if (res.status === 200) {
+      let user = await res.json();
+
+      console.log(user);
+
+      localStorage.setItem('user_id', user.id);
+      localStorage.setItem('username', user.username);
+
+    } else {
+      let error = await res.text();
+      console.log(error);
+    }
+  }
+
 
 }
