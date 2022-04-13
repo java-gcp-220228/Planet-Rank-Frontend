eb82a1b03f00ee9a18796593ad32ba688f8cdc94
Jack Ziegler <64660004+wumby@users.noreply.github.com>
2022-04-13 10:44:34 -0500

    refresh

--- src/app/models/exoplanetCard.ts
+++ src/app/models/exoplanetCard.ts
@@ -1,3 +1,12 @@
 export interface ExoplanetCards{
+    exoId: number;
+    planetName: string;
+    hostName: string;
+    numberOfPlanets: number;
+    numberOfStars: number;
+    discoverYear: number;
+    planetMass: number;
+    distanceFromEarth: number;
+    
     
 }
