5cea5def78aef9be690535aa8454fed29ce41e3e
Jack Ziegler <64660004+wumby@users.noreply.github.com>
2022-04-16 21:24:48 -0500

    fix url

--- src/app/services/nasa-image-api.service.ts
+++ src/app/services/nasa-image-api.service.ts
@@ -2,13 +2,13 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Image } from '../models/app.model.image'
-
+import { environment } from 'environments/environment';
 
 
 @Injectable()
 export class NasaImageApiService {
 
-  db_api = "http://localhost:8080/images";
+  db_api = `${environment.BACKEND_URL}/images`;
   constructor(private http: HttpClient) { }
 
   getImages(): Observable<Image[]> {
